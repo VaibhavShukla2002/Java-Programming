fa122276504fee2c6d57194a222006fcf2d0372a
Vaibhav Shukla <108264510+VaibhavShukla2002@users.noreply.github.com>
2022-10-22 11:39:45 +0530

    Add files via upload

---
+++ program to print the number from 1 to n using loops.java
@@ -0,0 +1,19 @@
+ import java.util.*;
+
+  public class Main{
+
+      public static void main(String Args[]){
+
+          int i=0;
+
+          while(i<11){
+
+              System.out.println(i);
+
+              i++;
+
+          }
+
+      }
+
+  }
