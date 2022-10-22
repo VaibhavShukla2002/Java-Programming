a0605ba5ff7f989c1c1fa4e8a495faa7f12ab6e1
Vaibhav Shukla <108264510+VaibhavShukla2002@users.noreply.github.com>
2022-10-22 11:20:51 +0530

    Add files via upload

---
+++ Sum of two numbers.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class Main{
+    public static void main(String Args[]){
+ 
+       Scanner object=new Scanner(System.in);
+
+        int a=24;
+
+        int b=25;
+
+        int sum=a+b;
+
+        {
+
+            System.out.println(sum);
+
+        }
+     }
+
+}
