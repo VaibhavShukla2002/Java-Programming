f14383aff7d0585cc8eb99f2cb51e7fa0b710a84
Vaibhav Shukla <108264510+VaibhavShukla2002@users.noreply.github.com>
2022-10-22 11:47:03 +0530

    Add files via upload

---
+++ Program to print numbers using do-while Loop.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+  public class Main{
+
+      public static void main(String Args[]){
+ 
+       int i=0;
+
+        do{
+
+            System.out.println(i);
+
+            i++;
+
+        }while(i<11);
+
+      }
+
+  }      
