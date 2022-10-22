f81f9b127c240554691622c861f90197e51fbc44
Vaibhav Shukla <108264510+VaibhavShukla2002@users.noreply.github.com>
2022-10-22 10:34:12 +0530

    Add files via upload

---
+++ Odd even program.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main{
+    
+public static void main(String[] Args){
+       
+ Scanner object = new Scanner(System.in);
+
+        int x = object.nextInt();
+
+        if(x%2==0){
+
+            System.out.println("Even");
+
+        }else{
+ 
+           System.out.println("Odd");
+ 
+       }
+ 
+   }
+
+}
