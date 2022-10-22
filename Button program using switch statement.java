0c194261d39d74baae8c401112926252c787f640
Vaibhav Shukla <108264510+VaibhavShukla2002@users.noreply.github.com>
2022-10-22 12:16:52 +0530

    Add files via upload

---
+++ Button program using switch statement.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main{
+
+    public static void main(String Args[]){
+
+        Scanner object=new Scanner(System.in);
+
+        int button=object.nextInt();
+
+        
+        
+    switch(button){
+
+        case 1:System.out.println("JAVA");
+
+        break;
+
+        case 2:System.out.println("PYTHON");
+
+        break;
+
+        case 3:System.out.println("JAVASCRIPT");
+
+        break;
+
+    }
+
+    }
+
+}
