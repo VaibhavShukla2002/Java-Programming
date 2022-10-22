637bd553669f6c63c5d8f0b6b92d9e0f3c3be622
Vaibhav Shukla <108264510+VaibhavShukla2002@users.noreply.github.com>
2022-10-22 10:43:37 +0530

    Add files via upload

---
+++ program of a and b (equal,greater , lesser).java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] Args){
+
+        Scanner object = new Scanner(System.in);
+
+        int a = object.nextInt();
+
+        int b = object.nextInt();
+
+        if(a==b){
+ 
+           System.out.println("Equal");
+
+        }else if(a<b){
+
+            System.out.println("a is lesser than b");
+
+        }else{
+
+            System.out.println("a is greater than b");
+
+        }
+
+    }
+
+}
