7c0d1d9893894f99a71bacfd4a1f9a4e532fd2cf
Vaibhav Shukla <108264510+VaibhavShukla2002@users.noreply.github.com>
2022-10-22 11:18:22 +0530

    Add files via upload

---
+++ Sum of two no if entered by user.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+public class Main{
+
+    public static void main(String Args[]){
+
+        Scanner object=new Scanner(System.in);
+
+        int a=object.nextInt();
+
+        int b=object.nextInt();
+
+        int sum=a+b;
+
+        {
+ 
+           System.out.println(sum);
+
+        }
+
+
+    }
+
+}
