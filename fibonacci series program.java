9f10c7a777feafe7a5492715868c711b7d921b18
Vaibhav Shukla <108264510+VaibhavShukla2002@users.noreply.github.com>
2022-11-12 11:44:39 +0530

    Add files via upload

---
+++ fibonacci series program.java
@@ -0,0 +1,28 @@
+//Fibonacci series program
+
+
+import java.util.*;
+
+public class Main{
+
+    public static void main(String Args[]){
+
+        int n1=0,n2=1,n3,i,count=05;
+
+        System.out.println(n1+" "+n2);
+
+        for(i=2;i<count;i++){
+
+            n3=n1+n2;
+
+            System.out.println(" "+n3);
+
+            n1=n2;
+
+            n2=n3;
+
+        }
+
+    }
+
+}
